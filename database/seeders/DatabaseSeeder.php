22a0b08ee10024bc96a07d1ffa63a04b1637d8e8
! fusY <onlyoneniqtophileee@gmail.com>
2024-09-13 09:33:55 +0800

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
+use App\Models\Country;
+use App\Models\Tag;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +23,18 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Country::create(['name' => 'Indonesia']);
+        Country::create(['name' => 'Malaysia']);
+        City::create(['country_id' => 1, 'name' => 'Samarinda']);
+        City::create(['country_id' => 1, 'name' => 'Jakarta']);
+        City::create(['country_id' => 1, 'name' => 'Balikpapan']);
+        City::create(['country_id' => 2, 'name' => 'Kuala-Lumpur']);
+        City::create(['country_id' => 2, 'name' => 'Johor']);
+        City::create(['country_id' => 2, 'name' => 'Sabah']);
+
+        Tag::create(['name' => 'Laravel', 'slug' => 'laravel']);
+        Tag::create(['name' => 'Vue JS', 'slug' => 'vue-js']);
+        Tag::create(['name' => 'Livewire', 'slug' => 'livewire']);
     }
 }
